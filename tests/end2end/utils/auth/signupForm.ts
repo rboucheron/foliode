f583f1a2a0a51d4c0d66b25746017129f1cadf3c
rboucheron <raphaelboucheron3@gmail.com>
2026-05-27 18:55:36 +0200

    feat-test-add-random-person

--- tests/end2end/utils/auth/signupForm.ts
+++ tests/end2end/utils/auth/signupForm.ts
@@ -1,15 +1,17 @@
 import { Page } from "playwright";
+import { generatePerson } from "./generatePerson";
 
 export const fillSignUpForm = async (page: Page) => {
+  const person = generatePerson();
   await page.getByRole("link", { name: "Inscription" }).click();
   await page.getByRole("textbox", { name: "Email Email*" }).click();
-  await page
-    .getByRole("textbox", { name: "Email Email*" })
-    .fill("raphaelboucheron+5@gmail.com");
+  await page.getByRole("textbox", { name: "Email Email*" }).fill(person.email);
   await page.getByRole("textbox", { name: "Prénom Prénom*" }).click();
-  await page.getByRole("textbox", { name: "Prénom Prénom*" }).fill("Rapha");
+  await page
+    .getByRole("textbox", { name: "Prénom Prénom*" })
+    .fill(person.lastName);
   await page.getByRole("textbox", { name: "Nom Nom*" }).click();
-  await page.getByRole("textbox", { name: "Nom Nom*" }).fill("Boucheron");
+  await page.getByRole("textbox", { name: "Nom Nom*" }).fill(person.firstName);
   await page
     .getByRole("textbox", { name: "Mot de passe Mot de passe*" })
     .click();
